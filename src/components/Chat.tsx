0a0c6e3f916ccc38c8dbbb8f489be9a4ae4f952c
hironomiu <hironomiu@gmail.com>
2022-01-08 14:02:26 +0900

    commited

--- src/components/Chat.tsx
+++ src/components/Chat.tsx
@@ -1,34 +1,10 @@
-import { FC, useEffect, memo } from 'react'
-import { useSelector, useDispatch } from 'react-redux'
-import {
-  selectIsAuthentication,
-  selectDisplayName,
-  selectUid,
-} from '../features/auth/authSlice'
-import { getFirebaseUser } from '../features/user/userSlice'
-import { setName, setUid, readMessages } from '../features/chat/chatSlice'
+import { FC, memo } from 'react'
+
 import { ChatList, ChatForm } from '.'
+import useChat from '../hooks/useChat'
 
 export const Chat: FC = memo(() => {
-  const dispatch = useDispatch()
-  const isAuthentication = useSelector(selectIsAuthentication)
-  const displayName = useSelector(selectDisplayName)
-  const uid = useSelector(selectUid)
-
-  useEffect(() => {
-    if (isAuthentication) {
-      dispatch(setName(displayName))
-      dispatch(setUid(uid))
-      dispatch(getFirebaseUser(uid))
-    }
-  }, [isAuthentication, displayName, dispatch, uid])
-
-  useEffect(() => {
-    if (isAuthentication) {
-      dispatch(readMessages())
-    }
-  }, [isAuthentication, dispatch])
-
+  useChat()
   return (
     <>
       <ChatList />
---
+++ src/hooks/useChat.tsx
@@ -0,0 +1,34 @@
+import { useEffect } from 'react'
+import { useSelector, useDispatch } from 'react-redux'
+import {
+  selectIsAuthentication,
+  selectDisplayName,
+  selectUid,
+} from '../features/auth/authSlice'
+import { getFirebaseUser } from '../features/user/userSlice'
+import { setName, setUid, readMessages } from '../features/chat/chatSlice'
+
+const useChat = () => {
+  const dispatch = useDispatch()
+  const isAuthentication = useSelector(selectIsAuthentication)
+  const displayName = useSelector(selectDisplayName)
+  const uid = useSelector(selectUid)
+
+  useEffect(() => {
+    if (isAuthentication) {
+      dispatch(setName(displayName))
+      dispatch(setUid(uid))
+      dispatch(getFirebaseUser(uid))
+    }
+  }, [isAuthentication, displayName, dispatch, uid])
+
+  useEffect(() => {
+    if (isAuthentication) {
+      dispatch(readMessages())
+    }
+  }, [isAuthentication, dispatch])
+
+  return
+}
+
+export default useChat

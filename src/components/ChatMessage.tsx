dc89e9dc3401d256e383aabcfa04ceb1239d79f0
hironomiu <hironomiu@gmail.com>
2022-03-24 08:00:50 +0900

    commited

--- src/components/ChatMessage.tsx
+++ src/components/ChatMessage.tsx
@@ -6,12 +6,23 @@ type Props = {
   createdAt: string
 }
 
+// TODO 切り出す
+const formatDate = (dt: Date) => {
+  const y = dt.getFullYear()
+  const m = ('00' + (dt.getMonth() + 1)).slice(-2)
+  const d = ('00' + dt.getDate()).slice(-2)
+  const h = ('00' + dt.getHours()).slice(-2)
+  const M = ('00' + dt.getMinutes()).slice(-2)
+  const s = ('00' + dt.getSeconds()).slice(-2)
+  return y + '-' + m + '-' + d + ' ' + h + ':' + M + ':' + s
+}
+
 export const ChatMessage: FC<Props> = memo(({ name, text, createdAt }) => {
   const dateTime = new Date(createdAt)
   return (
     <div className="flex flex-col m-2">
       <span className="text-gray-700 font-bold text-sm">
-        {name} {dateTime.toDateString()}
+        {name} {formatDate(dateTime)}
       </span>
       <span className="text-gray-500 text-base">{text}</span>
     </div>

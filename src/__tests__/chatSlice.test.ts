fa40652ec01051994fbd1c47a31f01d137335717
hironomiu <hironomiu@gmail.com>
2022-04-02 09:43:14 +0900

    commited

--- src/__tests__/chatSlice.test.ts
+++ src/__tests__/chatSlice.test.ts
@@ -6,26 +6,25 @@ import reducer, {
   setMessages,
   setReadMessageStatus,
 } from '../features/chat/chatSlice'
-
+const initialState: InitialState = {
+  sendMessage: {
+    uid: '',
+    name: '',
+    text: '',
+  },
+  messages: [
+    {
+      key: '',
+      name: '',
+      text: '',
+      createdAt: 0,
+      uid: '',
+    },
+  ],
+  readMessagesStatus: 'idle',
+}
 describe('chatSlice', () => {
   describe('setUid', () => {
-    const initialState: InitialState = {
-      sendMessage: {
-        uid: '',
-        name: '',
-        text: '',
-      },
-      messages: [
-        {
-          key: '',
-          name: '',
-          text: '',
-          createdAt: 0,
-          uid: '',
-        },
-      ],
-      readMessagesStatus: 'idle',
-    }
     it('test', () => {
       const action = {
         type: setUid.type,
@@ -34,5 +33,39 @@ describe('chatSlice', () => {
       const state = reducer(initialState, action)
       expect(state.sendMessage.uid).toEqual('dummy uid')
     })
+  })
+  describe('setName', () => {
+    it('test', () => {
+      const action = { type: setName.type, payload: 'dummy name' }
+      const state = reducer(initialState, action)
+      expect(state.sendMessage.name).toEqual('dummy name')
+    })
+  })
+  describe('setText', () => {
+    it('test', () => {
+      const action = { type: setText, payload: 'dummy text' }
+      const state = reducer(initialState, action)
+      expect(state.sendMessage.text).toEqual('dummy text')
+    })
+  })
+  describe('setMessages', () => {
+    const action = {
+      type: setMessages,
+      payload: [
+        {
+          key: 'dummy key',
+          name: 'dummy name',
+          text: 'dummy text',
+          createdAt: 1643665753272,
+          uid: 'dummy uid',
+        },
+      ],
+    }
+    const state = reducer(initialState, action)
+    expect(state.messages[0].key).toEqual('dummy key')
+    expect(state.messages[0].name).toEqual('dummy name')
+    expect(state.messages[0].text).toEqual('dummy text')
+    expect(state.messages[0].createdAt).toEqual(1643665753272)
+    expect(state.messages[0].uid).toEqual('dummy uid')
   })
 })

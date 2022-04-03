433aef487b6060875432c6f7f17f140a05ef5505
hironomiu <hironomiu@gmail.com>
2022-04-04 08:27:28 +0900

    commited

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -25,18 +25,36 @@ export const Profile: FC = memo(() => {
   }
   const user = useSelector(selectUser)
 
+  const handleClick = async () => {
+    console.log(user)
+    const sRef = ref(storage, 'images/' + user.uid + '_avatar')
+    console.log('called')
+    uploadBytes(sRef, image)
+      .then((snapshot) => {
+        console.log('ok')
+      })
+      .catch((err) => console.log(err))
+    getDownloadURL(ref(storage, 'images/' + user.uid + '_avatar')).then(
+      (url) => {
+        dispatch(updateFirebaseUser(user.uid, url))
+      }
+    )
+
+    // const url = await res(snapshot => snapshot.ref.getDownloadURL())
+    setModalOn(true)
+    setPreview('')
+    setImage('')
+    setKey((key) => key + 1)
+    setIsEntry((isEntry) => !isEntry)
+    // dispatch(updateFirebaseUser(user.uid, url))
+  }
+
   return (
     <div className="flex flex-col items-center ">
-      {modalOn ? (
-        <MessageModal
-          message="アバター画像を登録しました"
-          setModalOn={setModalOn}
-        />
-      ) : null}
       <div className="fixed top-20 ">
         <h1 className=" text-xl">{user.name}'s Profile</h1>
       </div>
-      <span>email:{user.email}</span>
+      <span className="my-4">email:{user.email}</span>
 
       <div className="flex -space-x-2 overflow-hidden flex-col px-4">
         {user.photoURL ? (
@@ -49,6 +67,12 @@ export const Profile: FC = memo(() => {
           'no photo image'
         )}
       </div>
+      {modalOn ? (
+        <MessageModal
+          message="アバター画像を登録しました"
+          setModalOn={setModalOn}
+        />
+      ) : null}
       {/* keyを設定することでアップロード後に選択をクリアする */}
       {isEntry ? (
         <>
@@ -68,33 +92,23 @@ export const Profile: FC = memo(() => {
               />
             ) : null}
           </div>
-          <button
-            onClick={async () => {
-              console.log(user)
-              const sRef = ref(storage, 'images/' + user.uid + '_avatar')
-              console.log('called')
-              uploadBytes(sRef, image)
-                .then((snapshot) => {
-                  console.log('ok')
-                })
-                .catch((err) => console.log(err))
-              getDownloadURL(
-                ref(storage, 'images/' + user.uid + '_avatar')
-              ).then((url) => {
-                dispatch(updateFirebaseUser(user.uid, url))
-              })
-
-              // const url = await res(snapshot => snapshot.ref.getDownloadURL())
-              setModalOn(true)
-              setPreview('')
-              setImage('')
-              setKey((key) => key + 1)
-              setIsEntry((isEntry) => !isEntry)
-              // dispatch(updateFirebaseUser(user.uid, url))
-            }}
-          >
-            アップロード
-          </button>
+          <div className="my-2">
+            <button
+              className="mx-2 px-2 py-1 border-2 rounded"
+              disabled={!preview ? true : false}
+              onClick={handleClick}
+            >
+              アップロード
+            </button>
+            <button
+              className="mx-2 px-2 py-1 border-2 rounded"
+              onClick={() => {
+                if (isEntry) setIsEntry((isEntry) => !isEntry)
+              }}
+            >
+              閉じる
+            </button>
+          </div>
         </>
       ) : (
         <span

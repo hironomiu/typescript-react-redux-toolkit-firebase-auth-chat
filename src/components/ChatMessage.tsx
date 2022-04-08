892ebf93c23a551d32e9236dbd63dc838f4cd1f8
hironomiu <hironomiu@gmail.com>
2022-04-08 18:18:10 +0900

    commited

--- src/components/ChatMessage.tsx
+++ src/components/ChatMessage.tsx
@@ -1,3 +1,4 @@
+import React from 'react'
 import { FC, memo } from 'react'
 import { formatDate } from '../lib/'
 
@@ -9,12 +10,19 @@ type Props = {
 
 export const ChatMessage: FC<Props> = memo(({ name, text, createdAt }) => {
   const dateTime = new Date(createdAt)
+  const output = text.split(/(\n)/).map((item, index) => {
+    return (
+      <React.Fragment key={index}>
+        {item.match(/\n/) ? <br /> : item}
+      </React.Fragment>
+    )
+  })
   return (
     <div className="flex flex-col m-2">
       <span className="text-gray-700 font-bold text-sm">
         {name} {formatDate(dateTime)}
       </span>
-      <span className="text-gray-500 text-base">{text}</span>
+      <span className="text-gray-500 text-base">{output}</span>
     </div>
   )
 })
